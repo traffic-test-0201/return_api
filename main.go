7ae54b001947976940da3b2e3f0187ab44c69a8d
dongdongjuhahaha <fjrzl2017@naver.com>
2023-01-29 13:18:30 +0900

    dockerfile test

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"github.com/dongdonjuhahaha/traffic/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/returns", controllers.GetReturn )
-	router.Run("localhost:9090")
+	router.GET("/returns", controllers.GetReturn)
+	router.Run(":9090")
 }
